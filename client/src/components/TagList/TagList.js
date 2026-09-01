dbe6b791a68f6bcadce3662092966ab3e97dc65e
leslieturai <turaileslie@gmail.com>
2026-09-01 15:14:15 -0500

    Tags added to TagList

--- client/src/components/TagList/TagList.js
+++ client/src/components/TagList/TagList.js
@@ -2,7 +2,43 @@
 export default function TagList () {
     return (
         <div className="tag-list-div">
-            <p>Tag list here</p>
+            <p>Flower</p>
+            <p>Plant</p>
+            <p>Sky</p>
+            <p>Landscape</p>
+            <p>Urban</p>
+            <p>Butterfly</p>
+            <p>Deer</p>
+            <p>Heron</p>
+            <p>Duck</p>
+            <p>Goose</p>
+            <p>Bird</p>
+            <p>Insect</p>
+            <p>Mushroom</p>
+            <p>Vehicle</p>
+            <p>Owl</p>
+            <p>Squirrel</p>
+            <p>Chipmunk</p>
+            <p>Moon</p>
+            <p>Frog</p>
+            <p>Fish</p>
+            <p>Snake</p>
+            <p>People</p>
+            <p>Family</p>
+            <p>Sun</p>
+            <p>Trailsign</p>
+            <p>Scrap</p>
+            <p>Tree</p>
+            <p>Shell</p>
+            <p>Dam</p>
+            <p>Spring</p>
+            <p>Summer</p>
+            <p>Fall</p>
+            <p>Winter</p>
+            <p>Sign</p>
+            <p>Water</p>
+            <p>Beach</p>
+            <p>Trail</p>
         </div>
     )
 }
