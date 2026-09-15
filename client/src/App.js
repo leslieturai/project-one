d6a6115fc3089f765ead42038811b462548b97d0
leslieturai <turaileslie@gmail.com>
2026-09-15 15:58:03 -0500

    Experimenting with FE processing of photoData

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { act, use, useEffect, useState } from "react";
 import YearSlice from "./components/YearSlice/YearSlice";
 import TagList from "./components/TagList/TagList";
 import Settings from "./components/Settings/Settings";
@@ -51,14 +51,26 @@ function App() {
         fetch("/dashboard").then(
           (res) => res.json()
         ).then((data) => {
-          setData(data)
+          //setData(data)
+          let tempData = []
+          tempData[0] = data[0]
+
+          data[0].forEach((timeFrame, i) => {
+            tempData.push(
+              data[2].filter((row) => row.Year == timeFrame)
+            )
+          })
+
+          console.log(tempData)
+
+          setData(tempData)
         })
       
     
   }, [])
 
   // Set limits for timeFrame
-  useEffect(() => {
+/*   useEffect(() => {
     // Default
     if (photoData[2] !== null && photoData[2] !== undefined && timeDepth !== null && timeDepth !== undefined) {
       if (timeDepth === 0 && activeLimit === null) {
@@ -67,7 +79,7 @@ function App() {
         setLimit(tempLimits)
       }
     }
-  }, [photoData])
+  }, [photoData]) */
 
 
 /*   useEffect(() => {
@@ -80,10 +92,17 @@ function App() {
           }
   }, [photoData]) */
 
+
   useEffect(() => {
-    if (photoData === null || photoData[2] === undefined || photoData == {} || photoData[2][0].Id === undefined) {
-      console.log("Nothing here")
-      
+    if (photoData[0] !== null && photoData[0] !== undefined) {
+      let tempLimits = []
+      photoData[0].forEach((elem) => tempLimits.push(10))
+      setLimit(tempLimits)
+    }
+  }, [photoData])
+
+  useEffect(() => {
+    if (photoData === null || photoData[2] === undefined || photoData == {} || photoData[2][0].Id === undefined) {      
       return
     }
 
@@ -106,6 +125,9 @@ function App() {
      setFrame(photoData[0][0])
   }
 }, [photoData]) */
+
+
+
 
 
 
@@ -250,140 +272,9 @@ function App() {
         )
       }
       
-      {/* 
-        OPTION 1:
-        Render first 10 of each timeFrame
-        Render the button at the end
-          If clicked, set next 10 of matching data to have a visible class
-            If there are fewer than 10 left, show that in the button's text
-        If there are none left, do not render the button
 
-        OPTION 2:
-        Render first 10 of each timeFrame
-          By breaking render loop via a frameLimit state variable
-        Render button at the end
-        If clicked, increment limit by 10
-          If there are fewer than 10 left, show that in the button's text
-          If there are none left, do not render the button
 
       
-      */}
-      
-        {
-        photoData[0] !== undefined ? 
-
-            ( 
-              photoData[0].map((timeFrame, i) => {
-                
-                return (
-
-
-                  <>
-
-                      {/* 
-                        If filtered, only show filtered elements and a header for what's being filtered for (this is done above)
-
-                        If there are no filters, show the normal page
-                          If timeDepth === 1, show the monthly view
-                          If timeDepth === 2, show the daily view of a month/year
-
-                        
-                      */}
-                    {
-                    timeDepth < 1 && filters === null ? (<h2 className="" key={i}
-                      onClick={(event) => {
-                          setDepth(timeDepth + 1)
-                          let queryString = new URLSearchParams(timeFrame).toString()
-                          let fullURL = "/time:" + queryString
-                          fetch(fullURL).then(
-                            (res) => res.json()
-                          ).then((data) => {
-                            setData(data)
-                          })
-                        }}
-                      >{timeFrame}</h2>) : 
-                      <h2 className={filters !== null ? "filtered" : ""} key={i}
-                          onClick={(event) => {
-                            setDepth(timeDepth + 1)
-                            let queryString = new URLSearchParams(timeFrame).toString()
-                            let queryEnd = new URLSearchParams(photoData[2][0].Year)
-                            let fullURL = "/test/:" + queryString + "/:" + queryEnd
-                            fetch(fullURL).then(
-                              (res) => res.json()
-                            ).then((data) => {
-                              setData(data)
-                            })
-                          }}
-                        style={activeIndex === null ? null : {pointerEvents: "none"}}
-                      >{timeFrame}</h2>
-                    }
-
-                    {
-                      
-                      photoData[2] !== undefined && activeLimit !== undefined && activeLimit !== null ? 
-                          photoData[2].map((row, j) => {
-
-                          if (timeDepth === 0) {
-                            if (row.Year == timeFrame) {
-                              return <YearSlice key={j} img={row} 
-                                handleIndex={IndexHandler}
-                                mapIndex={j}
-                                activeIndex={activeIndex}
-                              ></YearSlice>
-                            } else {
-                              return <></>
-                            }
-                          }
-
-                          if (timeDepth === 1) {
-                            if (row.Month == timeFrame) {
-                              if (filters !== null) {
-                                if (
-                                  [row.Tags].some(r => filters.includes(r))
-                                ) {
-                                return <YearSlice key={j} img={row} 
-                                handleIndex={IndexHandler}
-                                mapIndex={j}
-                                activeIndex={activeIndex}
-                              ></YearSlice>
-                                } else {
-                                  return <></>
-                                }
-                              } else {
-                                return <YearSlice key={j} img={row} 
-                                handleIndex={IndexHandler}
-                                mapIndex={j}
-                                activeIndex={activeIndex}
-                              ></YearSlice>
-                              }
-                            } else {
-                              return <></>
-                            }
-                          }
-
-                          if (timeDepth === 2) {
-                            if (row.Day == timeFrame) {
-                              return <YearSlice key={j} img={row} 
-                                handleIndex={IndexHandler}
-                                mapIndex={j}
-                                activeIndex={activeIndex}
-                              ></YearSlice>
-                            } else {
-                              return <></>
-                            }
-                          }
-
-
-                        })
-                      : <p style={activeIndex === null ? null : {pointerEvents: "none"}}>Loading...</p>
-                    }
-                  </>
-                )
-              })
-            )
-
-        : <p style={activeIndex === null ? null : {pointerEvents: "none"}}>Loading...</p>
-      }
 
     </div>
   );
